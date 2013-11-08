ce10d3bc5ce675dc649a4383c2a292e19314409a
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2013-11-08 10:28:29 -0800

    Fixing modeling bug in through association

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,8 @@
 class Employee < ActiveRecord::Base
   has_many :shift_assignments
   has_many :shift_exceptions
+  has_many :skill_assignments
+  has_many :location_assignments
   has_many :skills, through: :skill_assignments
   has_many :locations, through: :location_assignments
   has_many :wages
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,5 @@
 class Skill < ActiveRecord::Base
+  has_many :skill_assignments
   has_many :employees, through: :skill_assignments
   has_many :shifts
 end
