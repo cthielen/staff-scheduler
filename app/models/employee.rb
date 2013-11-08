87b04451e1ec587a1adf851d43c1d026e40b5fe1
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2013-11-08 14:07:48 -0800

    Adding employee to shift ER modelling

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,6 +5,7 @@ class Employee < ActiveRecord::Base
   has_many :location_assignments
   has_many :skills, through: :skill_assignments
   has_many :locations, through: :location_assignments
+  has_many :shifts, through: :shift_assignments
   has_many :wages
   has_many :employee_availabilities
   
