37579cc88dc443bb1a0742de5d2eda75f9085352
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2013-11-13 15:34:58 -0800

    Adding to employee tests, fixing whitespace

--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -1,8 +1,9 @@
 require 'test_helper'
 
 class EmployeeTest < ActiveSupport::TestCase
+
   test "Should be able to access Employee associated models" do
-    e = Employee.new
+    e = Employee.find(1)
     e.skill_assignments
     e.skills
     e.shift_assignments
@@ -24,29 +25,28 @@ class EmployeeTest < ActiveSupport::TestCase
     e = Employee.find(1)
     s = e.shift_assignments
     e.destroy
-    assert !s.exists?, "Shift assignments still exist after employee was destroyed"
+    assert !s.exists?, "shift_assignments still exist after employee was destroyed"
   end
 
   test "Should destroy shift_exceptions when employee is destroyed" do
     e = Employee.find(1)
     s = e.shift_exceptions
     e.destroy
-    assert !s.exists?, "Shift assignments still exist after employee was destroyed"
+    assert !s.exists?, "shift_exceptions still exist after employee was destroyed"
   end  
   
   test "Should destroy skill_assignments when employee is destroyed" do
     e = Employee.find(1)
     s = e.skill_assignments
     e.destroy
-    assert !s.exists?, "Shift assignments still exist after employee was destroyed"
+    assert !s.exists?, "skill_assignments still exist after employee was destroyed"
   end
   
   test "Should destroy location_assignments when employee is destroyed" do
     e = Employee.find(1)
     l = e.location_assignments
     e.destroy
-    assert !l.exists?, "Shift assignments still exist after employee was destroyed"
+    assert !l.exists?, "location_assignments still exist after employee was destroyed"
   end  
-  
   
 end
