6072cab5e91274893d0d53d405552fa29c568e90
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2013-11-13 12:46:51 -0800

    Fixing bug in model relationship

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,6 @@
 class Location < ActiveRecord::Base
   has_many :employees, through: :location_assignments
-  has_many :shifts, touch: true
+  has_many :shifts
   
   validates :name, presence: true, uniqueness: true
 
