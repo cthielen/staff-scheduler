7a08d61e405faf21935f02ec78f9097e2bb78cb2
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2013-11-13 16:02:55 -0800

    Adding schedule tests

--- test/models/schedule_test.rb
+++ test/models/schedule_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class ScheduleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Should be able to access Schedule associated models" do
+    s = Schedule.find(1)
+    s.shifts
+  end
+  
+  test "Should destroy shifts when schedule is destroyed" do
+    s = Schedule.find(1)
+    sh = s.shifts
+    s.destroy
+    assert !sh.exists?, "shifts still exist after schedule was destroyed"
+  end
+   
 end
