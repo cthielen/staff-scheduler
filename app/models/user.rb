223076685fa46857fd9593e1f59338f17c2955ec
Obada Kadri <okadri@ucdavis.edu>
2013-11-18 12:10:52 -0800

    Return role_symbols from RM

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,35 @@ class User < ActiveRecord::Base
   belongs_to :employee
   
   validates :loginid, :is_manager, presence: true 
+  
+  def role_symbols
+    require 'net/http'
+    require 'json'
+    require 'yaml'
+
+    # In case you receive SSL certificate verification errors
+    require 'openssl'
+    
+    uri = URI(DSS_RM_SETTINGS['HOST'] + "/people/#{loginid}.json")
+    req = Net::HTTP::Get.new(uri)
+    req['Accept'] = "application/vnd.roles-management.v1"
+    req.basic_auth(DSS_RM_SETTINGS['USER'], DSS_RM_SETTINGS['PASSWORD'])
+
+    begin
+      # Fetch URL
+      resp = Net::HTTP.start( uri.hostname, uri.port, use_ssl: true ) { |http|
+        http.request(req)
+      }
+      # Parse results
+      buffer = resp.body
+      result = JSON.parse(buffer)
+
+      return result["role_assignments"]
+        .find_all{ |r| r["application_id"] == STAFF_SCHEDULER_RM_ID }
+        .map{ |r| r["token"].to_sym }
+    rescue StandardError => e
+      $stderr.puts "Could not fetch RM URL #{e}"
+      return false
+    end
+  end
 end
---
+++ config/initializers/dss_rm.rb
@@ -0,0 +1,10 @@
+DSS_RM_SETTINGS_FILE = Rails.root.join( "config", "dss_rm.yml")
+STAFF_SCHEDULER_RM_ID = 20
+# OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
+if File.file?(DSS_RM_SETTINGS_FILE)
+  DSS_RM_SETTINGS = YAML.load_file(DSS_RM_SETTINGS_FILE)
+else
+  puts "You need to set up config/dss_rm.yml before running this application."
+  exit
+end
