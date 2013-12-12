dd1b367ff8b3b5ff0d647a9ee2c68abc56e7317e
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2013-12-12 09:48:44 -0800

    Updated state machine logic, fixed syntax error

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -9,42 +9,43 @@ class Schedule < ActiveRecord::Base
   # Notes: Transition Actions go on 'exits' not 'beginnings'
   
   # Schedule in progress (shifts being created)
-  INCOMPLETE_SCHEDULE = 0
+  INCOMPLETE_SCHEDULE = 1
   # Action: If schedule is empty, copy old schedule shifts
   # Action: refuse to create if overlaps with other schedule 
-  # Exit Condition: 'Complete' button clicked in schedule planning form
+  # Exit Condition: Change to state 2 when 'complete' button clicked in schedule planning form
   # Exit Command: Email employees a link to the availability creation form
   
   # Initial state after schedule creation
-  PENDING_AVAILABILITIES = 1
-  # Exit Condition: Change to state 2 when all active employee availabilities have been accounted for.  
+  PENDING_AVAILABILITIES = 2
+  # Exit Condition: Change to state 3 when all active employee availabilities have been accounted for.  
   # Exit Command: Email managers that all availabilities have been received and shift_assignments can be filled in
   
   # Ready to have shift_assignments associated to schedule
-  PENDING_ASSIGNMENTS = 2
-  # Exit Condition: Change to state 3 when the 'complete' button on the Schedule Planner form is clicked.  
+  PENDING_ASSIGNMENTS = 3
+  # Exit Condition: Change to state 4 when the 'complete' button on the Schedule Planner form is clicked.  
   # Exit Command: Email employees with uncomfirmed shift_assignments a link to the shift_assignment confirmation form
   
   # Shift_assignments in schedule need to be confirmed
-  PENDING_CONFIRMATION = 3
-  # Exit Condition: Change to state 4 when all shift_assignments associated to schedule have been confirmed.  
+  PENDING_CONFIRMATION = 4
+  # Exit Condition: Change to state 5 when all shift_assignments associated to schedule have been confirmed.  
   # Exit Command: Email managers a notification that the schedule is ready
 
   # All shift_assignments are confirmed
-  READY = 4
-  # Exit condition: Change to state 5 when schedule start date is reached
+  READY = 5
+  # Exit condition: Change to state 6 when schedule start date is reached
 
   # Schedule start date has been reached
-  ACTIVE = 5
-  # Action: Shift_assignment Absence generated - email all employees a link to the schedule
+  ACTIVE = 6
+  # Action: Shift_assignment Absence generated - email all employees a link to the schedule, (email affected employee in case it was made by manager and for record keeping)
   # Action: Unfilled Shift_exception in schedule tomorrow - email managers a notification
-  # Action: Shift_exception absence created - email employee (In both cases, when created by manager or employee)
-  # Exit Condition: Change to state 3 when Availability and shift_assignment conflict is detected - email managers a notification
-  # Exit Condition: Change to state 3 when new shift_assignment detected 
-  # Exit Condition: Change to state 6 when schedule end_date is passed
-  
+  # Action: Employee changes availability - create necessary 'absences' for assigned shifts that no longer match
+  # Action: Manager assigns shift_assignment to employee - email person for approval
+  # Action: Manager creates shift without assigning an employee - email all employees to pick up shifts
+  # Action: Manager creates shift and assigns an employee - email employee for confirmation
+  # Exit Condition: Change to state 7 when schedule end_date is passed
+
   # Schedule no longer in use
-  INACTIVE = 6
+  INACTIVE = 7
   
   # Runs state machine 'loop' of currently active schedule
   def process_state
@@ -74,7 +75,7 @@ class Schedule < ActiveRecord::Base
   end
   
   def trigger_shift_assignments_created
-  def
+  end
       
   # Action requests: Email links to forms to trigger human action
         
@@ -114,4 +115,4 @@ class Schedule < ActiveRecord::Base
   
   def check_shift_assignment_confirmations
   end
-  
+end
