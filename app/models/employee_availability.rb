c1595a6106cc7f2344e6ac3f1abd9cd559b2cc9a
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2014-01-08 13:54:01 -0800

    Linking employee availabilities to schedules

--- app/models/employee_availability.rb
+++ app/models/employee_availability.rb
@@ -2,6 +2,7 @@ class EmployeeAvailability < ActiveRecord::Base
   using_access_control
   
   belongs_to :employee
+  belongs_to :schedule
 
-  validates :start_datetime, :end_datetime, presence: true
+  validates :start_datetime, :end_datetime, :schedule_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140108004002) do
+ActiveRecord::Schema.define(version: 20140108214339) do
 
   create_table "employee_availabilities", force: true do |t|
     t.datetime "start_datetime"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140108004002) do
     t.integer  "employee_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "schedule_id"
   end
 
   create_table "employees", force: true do |t|
