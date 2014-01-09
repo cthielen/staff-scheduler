05dc1a199f8db2b77d7d96058903f3419c282d89
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2014-01-08 17:13:40 -0800

    Adding auto-notification when shift_assignment set to absence

--- app/models/shift_assignment.rb
+++ app/models/shift_assignment.rb
@@ -1,9 +1,20 @@
 class ShiftAssignment < ActiveRecord::Base
   using_access_control
+  after_save :notify_absence
   
   belongs_to :employee
   belongs_to :shift
-  belongs_to :shift_assignment_status
+  belongs_to :shift_assignment_status, :foreign_key => 'status_id'
   
-  validates :start_datetime, :end_datetime, :employee_id, :is_confirmed, :status_id, :shift_id, presence: true
+  validates :start_datetime, :end_datetime, :employee_id, :status_id, :shift_id, presence: true
+  validates :is_confirmed, :inclusion => {:in => [true, false]}
+  
+  def notify_absence
+    if self.shift_assignment_status.name == "absence"
+      message = "You have been recorded as absent from your shift assignment at Location: <location>, Skill: <skill>, start time: <start_time>, duration: <duration>. " 
+      recipient = self.employee
+      subject = "Shift Absence Notification"
+      StaffMailer.send_email(message, recipient, subject).deliver
+    end
+  end
 end
