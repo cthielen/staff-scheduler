20a7a826069fee38ca03d8235965941572311f00
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2014-01-30 14:35:20 -0800

    Adding validations for shift_assignments

--- app/models/shift_assignment.rb
+++ app/models/shift_assignment.rb
@@ -5,7 +5,8 @@ class ShiftAssignment < ActiveRecord::Base
   belongs_to :employee
   belongs_to :shift
   belongs_to :shift_assignment_status, :foreign_key => 'status_id'
-  
+
+  validate :shift_assignment_must_fit_inside_shift, :end_date_must_be_later_than_start_date
   validates :start_datetime, :end_datetime, :employee_id, :status_id, :shift_id, presence: true
   validates :is_confirmed, :inclusion => {:in => [true, false]}
   
@@ -13,6 +14,20 @@ class ShiftAssignment < ActiveRecord::Base
     if self.shift_assignment_status.name == "absence"
       recipients = self.employee.pluck(:email)
       StaffMailer.notify_absence(recipients).deliver
+    end
+  end
+  
+  def end_date_must_be_later_than_start_date
+    if self.end_datetime < self.start_datetime
+      errors.add(:end_date, "End date must come after start date")
+    end
+  end
+  
+  def shift_assignment_must_fit_inside_shift
+    if self.start_datetime < self.shift.start_datetime
+      errors.add(:start_datetime, "shift_assignment start_datetime must fall wtihin its shift")
+    elsif self.end_datetime > self.shift.end_dateime
+      errors.add(:end_datetime, "shift_assignment end_datetime must fall wtihin its shift")    
     end
   end
 end
