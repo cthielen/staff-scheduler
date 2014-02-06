12a6807c24274a4221353c78a469db7915ce86ec
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2014-02-06 03:44:20 -0800

    Fixing model association bug

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,7 +2,7 @@ class Skill < ActiveRecord::Base
   using_access_control
   
   has_many :skill_assignments, dependent: :destroy
-  has_many :employees, through: :skill_assignments
+  has_many :employees, :through => :skill_assignments
   has_many :shifts
   
   validates :title, presence: true, uniqueness: true
