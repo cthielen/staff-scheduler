97f4bb79a66f9cc47c1de1b4e98f0673b89192d5
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2014-02-06 21:53:53 -0800

    Updating documentation of shift assignment statuses

--- app/models/shift_assignment_status.rb
+++ app/models/shift_assignment_status.rb
@@ -4,12 +4,13 @@ class ShiftAssignmentStatus < ActiveRecord::Base
   has_many :shift_assignments
   
   validates :name, presence: true
-  # Suggest Statuses:
-  
-  # Scheduled
-  # Sick
-  # Absent
-  # Vacation
-  # Worked
-  
+  # Available Statuses:
+  # Planned -- assigned, has not yet occured
+  # Completed -- assigned, confirmed, and date has passed
+  # Rejected -- for when a shift_assignment is given to a person by a manager, but they do not confirm it and cancel the assignment reject it.
+  # Rejected is also used for the situation where an assignment is not confirmed, and the date passes. Auto-rejects.
+  # Sick -- un-assigned a confirmed assignment, for sickness
+  # Absent -- un-assigned a confirmed assignment, ahead of time
+  # Vacation -- un-assigneda confirmed assignment, for vacation
+  # No_Show -- un-assigned a confirmed assignment, employee didn't show up for their shift
 end
