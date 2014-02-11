a11f418b288f4fd303e8308607d88cf3fce3df23
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2014-02-10 17:09:00 -0800

    Adding test for schedule_overlap validation bug

--- test/models/schedule_test.rb
+++ test/models/schedule_test.rb
@@ -3,15 +3,27 @@ require 'test_helper'
 class ScheduleTest < ActiveSupport::TestCase
 
   test "Should be able to access Schedule associated models" do
-    s = Schedule.find(1)
-    s.shifts
+    without_access_control do
+      s = Schedule.find(1)
+      s.shifts
+    end
   end
   
   test "Should destroy shifts when schedule is destroyed" do
-    s = Schedule.find(1)
-    sh = s.shifts
-    s.destroy
-    assert !sh.exists?, "shifts still exist after schedule was destroyed"
+    without_access_control do
+      s = Schedule.find(1)
+      sh = s.shifts
+      s.destroy
+      assert !sh.exists?, "shifts still exist after schedule was destroyed"
+    end
   end
-   
+    
+  test "schedules_cannot_overlap should exclude comparing the saved schedule against itself" do
+    without_access_control do
+      s = Schedule.find(1)
+      assert s.present?, "Schedule was not found in fixture"
+      s.end_date = s.end_date - 1.days
+      assert s.save, "Validation should not compare schedule against itself after a change, #{s.inspect}"
+    end
+  end
 end
