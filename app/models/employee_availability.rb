86fd90ea0e1b5527b1906e47db50c8b041758089
Obada Kadri <okadri@ucdavis.edu>
2014-02-27 12:49:33 -0800

    Scoped EmployeeAvailability query to limit results to requested location and skill

--- app/models/employee_availability.rb
+++ app/models/employee_availability.rb
@@ -9,6 +9,8 @@ class EmployeeAvailability < ActiveRecord::Base
   validates :start_datetime, :end_datetime, :schedule_id, presence: true
 
   scope :by_schedule, lambda { |schedule| where(schedule_id: schedule) unless schedule.blank? }
+  scope :by_skill, lambda { |skill| joins(:employee => :skills).where('skill_assignments.skill_id = ?', skill).uniq unless skill.blank? }
+  scope :by_location, lambda { |location| joins(:employee => :locations).where('location_assignments.location_id = ?', location).uniq unless location.blank? }
 
   def compact_availabilities
     EmployeeAvailability.where(schedule_id: self.schedule_id, employee_id: self.employee_id).each do |availability|
