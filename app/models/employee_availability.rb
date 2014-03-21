e5d16329bc53c494656847d24c256891e94f6654
Obada Kadri <okadri@ucdavis.edu>
2014-03-21 15:45:06 -0700

    Added a validation for availabilities that they need to fall with a shift

--- app/models/employee_availability.rb
+++ app/models/employee_availability.rb
@@ -7,11 +7,22 @@ class EmployeeAvailability < ActiveRecord::Base
   belongs_to :schedule
 
   validates :start_datetime, :end_datetime, :schedule_id, presence: true
+  validate :availability_should_fall_within_a_defined_shift
 
   scope :by_schedule, lambda { |schedule| where(schedule_id: schedule) unless schedule.blank? }
   scope :by_skill, lambda { |skill| joins(:employee => :skills).where('skill_assignments.skill_id = ?', skill).uniq unless skill.blank? }
   scope :by_location, lambda { |location| joins(:employee => :locations).where('location_assignments.location_id = ?', location).uniq unless location.blank? }
 
+  def availability_should_fall_within_a_defined_shift
+    self.schedule.shifts.each do |shift|
+      if self.start_datetime >= shift.start_datetime and self.end_datetime <= shift.end_datetime
+        # A shift was found, save is allowed
+        return
+      end
+    end
+    # If no shifts found for the selected range, add the error
+    errors.add(:outside_shift, "Availability must fall wtihin a shift")
+  end
   def compact_availabilities
     EmployeeAvailability.where(schedule_id: self.schedule_id, employee_id: self.employee_id).each do |availability|
       if (availability.start_datetime == end_datetime) 
